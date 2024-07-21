490ad12054fb45eb9b6e1d2ba07b3f338b1f3351
Gorchanyuk <gorchanyk@gmail.com>
2024-07-21 12:09:51 +0400

    Добавил конфигурацию серверов в swagger

--- metricsProducer/src/main/java/t1/gorchanyuk/metricsproducer/config/SwaggerConfiguration.java
+++ metricsProducer/src/main/java/t1/gorchanyuk/metricsproducer/config/SwaggerConfiguration.java
@@ -3,11 +3,20 @@ package t1.gorchanyuk.metricsproducer.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 public class SwaggerConfiguration {
+
+    @Value("${server.port}")
+    private String port;
+
     @Bean
     public OpenAPI apiDocConfig() {
         return new OpenAPI()
@@ -17,6 +26,9 @@ public class SwaggerConfiguration {
                         .version("1.0.0")
                         .contact(new Contact()
                                 .name("Gorchanyuk")
-                                .email("gorchanuk@gmail.com")));
+                                .email("gorchanuk@gmail.com")))
+                .servers(List.of(
+                        new Server().url("http://localhost:" + port)
+                ));
     }
 }
