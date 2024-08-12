65a64ff960608a0056d58ffa6f95893e0bbdff57
Gorchanyuk <gorchanyk@gmail.com>
2024-08-12 10:11:59 +0400

    Отредактировал mklib.sh согласно best practices

--- metricsProducer/mklib.sh
+++ metricsProducer/mklib.sh
@@ -9,86 +9,90 @@ CHART_FOLDER="./../chart"
 MY_VALUES="./../my.yaml"
 
 #Проверка, что находимся в мастере и что совпадаем с удаленным репо
-function is_master() {
+is_master() {
   # Получаем имя текущей ветки
+  local current_branch
+  local local_commit
+  local remote_commit
   current_branch=$(git rev-parse --abbrev-ref HEAD)
 
   # Проверяем, является ли текущая ветка мастер-веткой или основной веткой
-  if [ "$current_branch" != "master" ] && [ "$current_branch" != "main" ]; then
+  if [[ "${current_branch}" != "master" ]] && [[ "${current_branch}" != "main" ]]; then
     echo "Для сборки образа нужно находиться в ветке master или main"
     exit 1
   fi
 
   # Проверяем, что текущий коммит совпадает с удаленным
-  git fetch origin "$current_branch"
+  git fetch origin "${current_branch}"
   local_commit=$(git rev-parse HEAD)
-  remote_commit=$(git rev-parse origin/"$current_branch")
+  remote_commit=$(git rev-parse origin/"${current_branch}")
 
-  if [ "$local_commit" != "$remote_commit" ]; then
+  if [[ "${local_commit}" != "${remote_commit}" ]]; then
     echo "Локальная ветка не на последнем коммите"
     exit 1
   fi
 
-  echo "Находимся в ветке $current_branch и на последнем коммите"
+  echo "Находимся в ветке ${current_branch} и на последнем коммите"
 }
 
 #Проверка, что отсутствуют незафиксированные изменения
-function is_committed() {
+is_committed() {
   status_output=$(git status --porcelain | grep -cv '^??')
-  if [ "${status_output}" != 0 ]; then
+  if [[ "${status_output}" != 0 ]]; then
     echo "Есть незафиксированные изменения в проекте"
     exit 1
   fi
 }
 
 #Вычисляем хеш по файлам и пакетам
-function get_hash() {
-  declare -a my_array=("/src" "/pom.xml" "/Dockerfile")
-  total_hash=""
+get_hash() {
+  local my_array=("/src" "/pom.xml" "/Dockerfile")
+  local total_hash=""
 
   for element in "${my_array[@]}"; do
-    item="$1/$element"
-    if [ ! -e "$item" ]; then
+    local item="$1/${element}"
+    if [[ ! -e "${item}" ]]; then
       continue
     fi
-    hash=""
-    if [ -d "$item" ]; then
-      hash="$(find "$item" -type f -exec sha256sum {} + | awk '{ print $1 }')"
+    local hash=""
+    if [[ -d "${item}" ]]; then
+      hash="$(find "${item}" -type f -exec sha256sum {} + | awk '{ print $1 }')"
     else
-      hash="$(sha256sum "$item" | awk '{ print $1 }')"
+      hash="$(sha256sum "${item}" | awk '{ print $1 }')"
     fi
     total_hash+="${hash}"
   done
 
-  echo -n "$total_hash" | sha256sum | head -c 8
+  echo -n "${total_hash}" | sha256sum | head -c 8
 }
 
 # Проверяем наличие сборки в локальном репо
-function is_exist_local() {
+is_exist_local() {
   local image_name=$1
   local tag_name=$2
+  local result
 
-  result=$(docker images -q "$image_name":"$tag_name")
-  if [ -z "$result" ]; then
+  result=$(docker images -q "${image_name}":"${tag_name}")
+  if [[ -z "${result}" ]]; then
     return 1
   fi
 }
 
 # Проверяем наличие сборки в удаленном репо
-function is_exist_remote() {
+is_exist_remote() {
   local repository=$1
   local tag=$2
   local url="https://hub.docker.com/v2/repositories/${repository}/tags/${tag}"
   local http_code
 
-  http_code=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-  if [ "$http_code" != 200 ]; then
+  http_code=$(curl -s -o /dev/null -w "%{http_code}" "${url}")
+  if [[ "${http_code}" != 200 ]]; then
     return 1
   fi
 }
 
 # Проверяем наличие такой сборки
-function check_tag() {
+check_tag() {
   local repo=$1
   local tag=$2
   if is_exist_local "$repo" "$tag"; then
@@ -101,34 +105,38 @@ function check_tag() {
   fi
   echo "Сборка не найдена"
 }
+
 # Добавляет тег к комиту
-function add_tag() {
-  tag_name=$1
+add_tag() {
+  local tag_name=$1
   git tag -a "${tag_name}" -m "Соответствует образу сборки с тегом - ${tag_name}"
-  if [ $? -eq 1 ]; then
+  if [[ $? -eq 1 ]]; then
     echo "Тег ${tag_name} уже существует"
   fi
 }
 
 # Запуск helm чарта
-function helm_run() {
-  STANDID=$1
+helm_run() {
+  local STANDID=$1
+  local HELM_EXISTS
+
   echo "### HELM: update dependency  ============================================"
   #  TODO Нужно раскоментировать
   #    helm dependency update $STANDID
 
   HELM_EXISTS=$(helm ls | grep -c ${HELM_NAME})
-  if [ "${HELM_EXISTS}" == 0 ]; then
+  if [[ "${HELM_EXISTS}" == 0 ]]; then
     echo "### HELM: install  ====================================================="
-    helm install ${HELM_NAME} "$STANDID" -f $MY_VALUES
+    helm install "${HELM_NAME}" "${STANDID}" -f "${MY_VALUES}"
   else
     echo "### HELM: upgrade  ====================================================="
-    helm upgrade ${HELM_NAME} $STANDID -f $MY_VALUES
+    helm upgrade "${HELM_NAME}" "${STANDID}" -f "${MY_VALUES}"
   fi
 }
 
 # Финальная функция, запускает сборку
-function build_all() {
+build_all() {
+  local hash
   is_master && is_committed
 
   hash=$(get_hash "${CDIR}")
@@ -137,17 +145,17 @@ function build_all() {
   mvn docker:build -Dproject.image.name="${repoName}/${imageName}" -Dtag.version="${hash}"
   status=$?
 
-  if [ "${status}" -eq 1 ]; then
+  if [[ "${status}" -eq 1 ]]; then
     hash="Error_${hash}"
     echo "Произошла ошибка во время сборки образа"
   fi
 
   add_tag "${hash}"
 
-  if [ ! -f $MY_VALUES ]; then
-    touch $MY_VALUES
+  if [[ ! -f "${MY_VALUES}" ]]; then
+    touch "${MY_VALUES}"
   fi
-  IMAGE_VERSION="${repoName}/${imageName}:${hash}" yq e -i ".${chartName}.image = strenv(IMAGE_VERSION)" $MY_VALUES
+  IMAGE_VERSION="${repoName}/${imageName}:${hash}" yq e -i ".${chartName}.image = strenv(IMAGE_VERSION)" "${MY_VALUES}"
   helm_run "${CHART_FOLDER}"
 }
 
