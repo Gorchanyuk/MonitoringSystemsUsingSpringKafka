e0486ac4667c5ea0be61a03b646f76508e62c3d7
Gorchanyuk <gorchanyk@gmail.com>
2024-08-13 13:51:08 +0400

    Добавил кастомную настройку рессурсов для продюсера и настроил работу с ним через доккер

--- docker-init.sh
+++ docker-init.sh
@@ -3,8 +3,9 @@
 HELM_NAME=app
 CHART_FOLDER="/opt/my-chart/chart"
 MY_VALUES="/opt/my-chart/my.yaml"
+RESOURCES="/opt/my-chart/resources.yaml"
 
-cd /opt/my-chart || exit
+touch "${RESOURCES}"
 
 #Задаем конфигурацию для kubectl
 kubectl config set-cluster minikube --server=https://minikube:8443 --certificate-authority=/opt/config/ca.crt
@@ -12,12 +13,13 @@ kubectl config set-credentials minikube --client-key=/opt/config/client.key --cl
 kubectl config set-context minikube --cluster=minikube --user=minikube
 kubectl config use-context minikube
 
-
+cd /opt/my-chart || exit
 HELM_EXISTS=$(helm ls | grep -c ${HELM_NAME})
+
 if (( "${HELM_EXISTS}" == 0 )); then
   echo "### HELM: install  ====================================================="
-  helm install "${HELM_NAME}" "${CHART_FOLDER}" -f "${MY_VALUES}"
+  helm install "${HELM_NAME}" "${CHART_FOLDER}" -f "${MY_VALUES}" -f "${RESOURCES}"
 else
   echo "### HELM: upgrade  ====================================================="
-  helm upgrade "${HELM_NAME}" "${CHART_FOLDER}" -f "${MY_VALUES}"
+  helm upgrade "${HELM_NAME}" "${CHART_FOLDER}" -f "${MY_VALUES}" -f "${RESOURCES}"
 fi
